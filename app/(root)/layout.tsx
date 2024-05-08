a13ba3080ec86350523791847abae4bb872127b6
sraveshnandan <kumarsravesh39@gmail.com>
2024-05-08 16:33:35 +0530

    ui_update_001

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { Footer, NavBar } from "@/components";
 import { ClerkProvider } from "@clerk/nextjs";
-
 export const metadata: Metadata = {
   title: "XecueCode",
   description: "Next Gen Software Development",
@@ -13,14 +12,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`max-w-[1440px] w-screen mx-auto bg-primary text-white`}
-        >
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <section
+      className={`max-w-[1440px] w-screen mx-auto bg-primary text-white`}
+    >
+      <NavBar />
+      {children}
+      <Footer />
+    </section>
   );
 }
